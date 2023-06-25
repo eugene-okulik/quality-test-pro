b933f4f6489e6e3a956a5814ac3a018e3fc59c0b
sergey211 <sergey211@gmail.com>
2023-06-25 13:17:31 +0200

    fix of half of homework

--- homework/sergey_svetlichny/lesson2/task1.py
+++ homework/sergey_svetlichny/lesson2/task1.py
@@ -1,5 +1,5 @@
 a = int(input('input a '))
 b = int(input('input b '))
-print('a + b = '+str(a + b))
-print('a - b = '+str(a - b))
-print('a x b = '+str(a * b))
+print('a + b = ' + str(a + b))
+print('a - b = ' + str(a - b))
+print('a x b = ' + str(a * b))
