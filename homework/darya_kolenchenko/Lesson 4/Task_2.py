e3d829c999c3830d38571fbeb30e0988ed70c08f
danica2383 <130681348+danica2383@users.noreply.github.com>
2023-07-05 20:11:01 +0300

    Darya_K, lesson 4

---
+++ homework/darya_kolenchenko/Lesson 4/Task_2.py
@@ -0,0 +1,9 @@
+
+k = 14
+while True:
+    p = int(input('Угадай цифру: '))
+    if p == k:
+        print('Поздравляю! Вы угадали!')
+        break
+    elif p != k:
+        print('Попробуйте снова')
