15eb9b30aaad83020fd3433f8742a2d52e34b981
sergey211 <sergey211@gmail.com>
2023-07-07 10:53:41 +0200

    updated according to the comments

--- homework/sergey_svetlichny/lesson4/task1.py
+++ homework/sergey_svetlichny/lesson4/task1.py
@@ -8,20 +8,22 @@ print("Text started:")
 print(text)
 my_list = text.split()
 my_list_new = []
-for i in range(0, len(my_list)):
-    str1 = my_list[i]
-    if my_list[i][-1] == ',':
+# for i in range(0, len(my_list)):
+#     str1 = my_list[i]  # упрощаем:
+for str1 in my_list:
+    if str1[-1] == ',':
         my_list_new.append(str1.rstrip(str1[-1]) + 'ing, ')
-    elif my_list[i][-1] == '.':
+    elif str1[-1] == '.':
         my_list_new.append(str1.rstrip(str1[-1]) + 'ing. ')
     elif str1.__contains__(','):
         str2 = str1.replace(',', "ing, ")
         my_list_new.append(str2 + 'ing ')
     else:
-        my_list_new.append(my_list[i] + 'ing ')
-text_new = ''
-for i in range(0, len(my_list_new)):
-    text_new += my_list_new[i]
+        my_list_new.append(str1 + 'ing ')
+# text_new = ''
+# for i in range(0, len(my_list_new)):
+#     text_new += my_list_new[i]  # упрощаем эти 3 строки:
+text_new = ' '.join(my_list_new)
 print()
 print("Text updated:")
 print(text_new)
