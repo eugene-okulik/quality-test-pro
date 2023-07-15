f623efc5f7eeab6494c7a61baaf2181d65252968
danica2383 <130681348+danica2383@users.noreply.github.com>
2023-07-15 22:41:29 +0300

    Darya_K, lesson 6 (give up)

--- homework/darya_kolenchenko/Lesson 6/task 2.py
+++ homework/darya_kolenchenko/Lesson 6/task 2.py
@@ -3,15 +3,18 @@ def fib(n):
     for number in range(n):
         yield num_a
         num_a, num_b = num_b, num_a + num_b
+    for number in list(fib(n)):
+        if number == 5:
+            print(number)
 
 
-gen = list(fib(100001))
+# gen = list(fib(100001))
 # print(gen[5], gen[200], gen[1000], gen[100000])
 
 
 # print("%e" % gen[5], "%e" % gen[200], "%e" % gen[1000], "%e" % gen[100000])
 # бьет ошибку, форматирование не может обработать последнее число
 
-for count, num in enumerate(gen):
-    if count == 5:
-        print(count, num)
+# for count, num in enumerate(gen):
+#     if count == 5:
+#         print(count, num)
