9163fe87c19e8a5cf962e43be08b81cb324247a1
sergey211 <sergey211@gmail.com>
2023-07-20 16:52:32 +0200

    midterm commit (just for history)

--- homework/sergey_svetlichny/lesson7/task1.py
+++ homework/sergey_svetlichny/lesson7/task1.py
@@ -13,18 +13,16 @@ def inputdate():
     return date1
 
 
-def parsedate(date):
-    for x in range(0, len(parsesymbols)):
-        if date.__contains__(parsesymbols[x]):
+def parsedate():
+    for symbol in parsesymbols:
+        if date1.__contains__(symbol):
             global separ
-            separ = parsesymbols[x]
-            my_list: list[str] = date1.split(parsesymbols[x])
-            return my_list
+            separ = symbol
 
 
-def checklist(my_list, separ):
+def checklist():
     python_date = datetime.datetime.strptime(date1, '%d' + separ + '%m' + separ + '%Y')
-    print('Данная дата в формате питона выглядит так:: ')
+    print('Данная дата в формате питона выглядит так: ')
     print(python_date)
     global dategood
     dategood = True
@@ -32,12 +30,14 @@ def checklist(my_list, separ):
 
 inputdate()
 try:
-    checklist(parsedate(date1), separ)
+    parsedate()
+    checklist()
 except ValueError:
     while not dategood:
         try:
             print('Ввести дату необходимо в формате: 31-12-2023')
             inputdate()
-            checklist(parsedate(date1), separ)
+            parsedate()
+            checklist()
         except ValueError:
             print("Неправильный формат даты")
