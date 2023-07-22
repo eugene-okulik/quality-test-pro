6b44b1c52b8b6ee3ab5df71ac9e363ecfd361958
sergey211 <sergey211@gmail.com>
2023-07-22 06:58:29 +0200

    fixed according with comments

--- homework/sergey_svetlichny/lesson9/task1.py
+++ homework/sergey_svetlichny/lesson9/task1.py
@@ -1,15 +1,13 @@
 class Book:
-    page_material: str
-    is_text_present: bool
-    name: str
-    author: str
-    pages_qty: int
-    # isbn: str
-    reserved: bool
+    page_material = 'бумага'
+    is_text_present = True
+    name: ''
+    author: ''
+    pages_qty: 0
+    isbn: ''
+    reserved: False
 
-    def __init__(self, page_material, is_text_present, name, author, pages_qty, reserved):
-        self.page_material = page_material
-        self.is_text_present = is_text_present
+    def __init__(self, name, author, pages_qty, reserved):
         self.name = name
         self.author = author
         self.pages_qty = pages_qty
@@ -23,19 +21,19 @@ class Book:
             return ''
 
 
-book_1 = Book('бумага', True, 'Отверженные', 'Виктор Гюго', 496, False)
-book_2 = Book('бумага', True, 'Робинзон Крузо', 'Даниэдь Дефо', 274, False)
-book_3 = Book('бумага', False, 'Вокруг света за 80 дней', 'Жюль Верн', 405, False)
-Book.isbn = ''
-book_3.isbn = '978-5-9287-3204-2'
-book_4 = Book('бумага', True, 'Подземелья черного замка', 'Дмитрий Браславский', 342, True)
+book_1 = Book('Отверженные', 'Виктор Гюго', 496, False)
+book_2 = Book('Робинзон Крузо', 'Даниэдь Дефо', 274, False)
+book_3 = Book('Вокруг света за 80 дней', 'Жюль Верн', 405, False)
+book_4 = Book('Подземелья черного замка', 'Дмитрий Браславский', 342, False)
 book_4.isbn = '978-5-905195-11-2'
-book_5 = Book('бумага', True, 'На западном фронте без перемен', 'Эрих Мария Ремарк', 396, False)
+book_5 = Book('На западном фронте без перемен', 'Эрих Мария Ремарк', 396, False)
 
+book_3.isbn = '978-5-9287-3204-2'
+book_4.reserved = True
 book_list = (book_1, book_2, book_3, book_4, book_5)
 for x in book_list:
-    print('Название: ' + x.name, ', Автор: ' + x.author, ', страниц: ' + str(x.pages_qty),
-          ', материал: ' + x.page_material, x.is_reserved())
+    print(f'Название: {x.name}, Автор: {x.author}, страниц: {str(x.pages_qty)}'
+          f', материал: {x.page_material} {x.is_reserved()}')
 
 
 class Schoolbook(Book):
@@ -43,22 +41,22 @@ class Schoolbook(Book):
     group: str
     is_task_present: bool
 
-    def __init__(self, page_material, is_text_present, name, author, pages_qty, reserved, subject, group,
+    def __init__(self, name, author, pages_qty, reserved, subject, group,
                  is_task_present):
-        super().__init__(page_material, is_text_present, name, author, pages_qty, reserved)
+        super().__init__(name, author, pages_qty, reserved)
         super().is_reserved()
         self.subject = subject
         self.group = group
         self.is_task_present = is_task_present
 
 
-schoolbook_1 = Schoolbook('бумага', True, 'Алгебра', 'Перельман', 195, True, 'Математика', 9, True)
-schoolbook_2 = Schoolbook('бумага', False, 'Геометрия', 'Перельман', 206, False, 'Математика', 6, True)
-schoolbook_3 = Schoolbook('бумага', True, 'География Евразии', 'Птушкин', 274, True, 'География', 5, False)
-schoolbook_4 = Schoolbook('бумага', True, 'Информатика', 'Билл Гейтс', 332, False, 'Информатика', 8, False)
-schoolbook_5 = Schoolbook('бумага', True, 'Астрономия', 'Илон Маск', 184, True, 'Астрономия', 11, False)
+schoolbook_1 = Schoolbook('Алгебра', 'Перельман', 195, True, 'Математика', 9, True)
+schoolbook_2 = Schoolbook('Геометрия', 'Перельман', 206, False, 'Математика', 6, True)
+schoolbook_3 = Schoolbook('География Евразии', 'Птушкин', 274, True, 'География', 5, False)
+schoolbook_4 = Schoolbook('Информатика', 'Билл Гейтс', 332, False, 'Информатика', 8, False)
+schoolbook_5 = Schoolbook('Астрономия', 'Илон Маск', 184, True, 'Астрономия', 11, False)
 print()
 schoolbook_list = (schoolbook_1, schoolbook_2, schoolbook_3, schoolbook_4, schoolbook_5)
 for x in schoolbook_list:
-    print('Название: ' + x.name, ', Автор: ' + x.author, ', страниц: ' + str(x.pages_qty),
-          ', предмет: ' + x.subject, ', класс: ' + str(x.group), x.is_reserved())
+    print(f'Название: {x.name}, Автор: {x.author}, страниц: {str(x.pages_qty)}'
+          f', предмет: {x.subject}, класс: {str(x.group)} {x.is_reserved()}')
