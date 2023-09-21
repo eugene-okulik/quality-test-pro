e180502cecee64fc46297ca8854489f19151405a
Sergey <sergey211@gmail.com>
2023-09-21 12:58:53 +0200

    Analyzer, fixed some troubles (but not all yet)

--- homework/sergey_svetlichny/logs_analyzer/analizer.py
+++ homework/sergey_svetlichny/logs_analyzer/analizer.py
@@ -15,9 +15,9 @@ from os import walk
 
 # from pack.check_date import checklist
 
-my_dict = {}
-check_res = []
-l1 = []
+parsed_data = {}
+inputted_date_analizer = []
+index_of_string_to_find = []
 # counter = 0
 # date_lines = []
 # lines_with_date = []
@@ -42,9 +42,9 @@ isFile = os.path.isfile(args.file)
 # print('The file present at the path is a regular file:', isFile)
 if not isFile:
     isDirectory = os.path.isdir(fpath)
-    print('The path is a directory:', isDirectory)
-    if not isDirectory:
-        print('Not a File and not a Dir')
+    # print('The path is a directory:', isDirectory)
+    # if not isDirectory:
+        # print('Not a File and not a Dir')
 
 date_lines = []
 
@@ -54,30 +54,45 @@ type_of_date = []  # not_date, good_d, less, more, range_d
 
 def parse_separ(date_to_parse):
     for symbol in parse_symbols:
-        if date_to_parse.__contains__(symbol):
+        if symbol in date_to_parse:
             # return date_to_parse, symbol
             return symbol
     return
 
 
-def checklist(dat: str):
+def check_date_length(dat: str):
     if len(dat) == 20:
         if dat.endswith('+'):
             dat = dat[:19]
-            return [check10(dat), 'more']
+            return [check_inputted_date(dat), 'more']
         elif dat.endswith('-'):
             dat = dat[:19]
-            return [check10(dat), 'less']
+            return [check_inputted_date(dat), 'less']
     if len(dat) == 19:
-        return [check10(dat), 'date_ok']
+        return [check_inputted_date(dat), 'date_ok']
 
     if len(dat) == 39:
         date1 = dat[0:19]
         date2 = dat[20:39]
-        return [check10(date1), 'range_d', check10(date2)]
+        return [check_inputted_date(date1), 'range_d', check_inputted_date(date2)]
+
+    if len(dat) == 11:
+        if dat.endswith('+'):
+            dat = dat[:10]+'-00-00-00'
+            return [check_inputted_date(dat), 'more']
+            # return [dat, 'more']
+        elif dat.endswith('-'):
+            dat = dat[:10]+'-00-00-00'
+            return [check_inputted_date(dat), 'less']
+            # return [dat, 'less']
+    if len(dat) == 10:
+        dat1 = dat+'-00-00-00'
+        dat2 = dat+'-23-59-59'
+        return [check_inputted_date(dat1), 'range_d', check_inputted_date(dat2)]
+        # return [dat, 'date_ok']
 
 
-def check10(dat):
+def check_inputted_date(dat):
     separator = parse_separ(dat)
     python_date = datetime.datetime.strptime(dat,
                                              f'%d{separator}%m{separator}%Y{separator}%H{separator}%M{separator}%S')
@@ -97,33 +112,34 @@ def file_open(fil):
             time.sleep(0)
             list_of_words = my_line.split()
             first_word = list_of_words[0]
-            if len(first_word) == 10 and first_word.__contains__('-'):
+            if len(first_word) == 10 and '-' in first_word:
+            # if len(first_word) == 10 and first_word.__contains__('-'):
                 date_lines.append(my_counter)
                 # print(line[:23])
                 python_date2 = datetime.datetime.strptime(my_line[:23], '%Y-%m-%d %H:%M:%S.%f')
                 # print(python_date2)
                 # print(line[24:150].rstrip())
                 if args.full:
-                    my_dict.update({previous_date: my_str[47:]})
-                    # my_dict.update({previous_date: my_str})
+                    parsed_data.update({previous_date: my_str[47:]})
+                    # parsed_data.update({previous_date: my_str})
                     my_str = my_line.rstrip()
-                    my_dict[python_date2] = my_line[47:].rstrip()
+                    parsed_data[python_date2] = my_line[47:].rstrip()
                 else:
-                    my_dict.update({previous_date: my_str[47:197]})
+                    parsed_data.update({previous_date: my_str[47:197]})
                     my_str = my_line[:300].rstrip()
-                    my_dict[python_date2] = my_line[47:300].rstrip()
-                # my_dict[line[:23]] = line[47:197].rstrip()
+                    parsed_data[python_date2] = my_line[47:300].rstrip()
+                # parsed_data[line[:23]] = line[47:197].rstrip()
                 previous_date = python_date2
             else:
                 if args.full:
                     my_str += my_line
-        my_dict.pop('', )
-    file.close()
+        parsed_data.pop('', )
+    # file.close()
 
 
 if isDirectory:
     filenames = next(walk(args.file), (None, None, []))[2]  # [] if no file
-    print(filenames)
+    # print(filenames)
     for every_file in filenames:
         file_open(os.path.join(args.file, every_file))
     # print('Date lines:', date_lines)
@@ -150,77 +166,84 @@ def find_all_indexes(input_str, search_str):
 # case 1 (UNWANTED)
 if args.unwanted is not None:
     # print('case 1')
-    for k, v in sorted(my_dict.items()):
-        if v.lower().__contains__(args.unwanted):
-            my_dict.pop(k)
+    for k, v in sorted(parsed_data.items()):
+        # if v.lower().__contains__(args.unwanted):
+        if args.unwanted in v.lower():
+            parsed_data.pop(k)
 
 # case 2 (DATE WITHOUT TEXT)
 # if args.date is not None and args.text is None:
 if args.date is not None:
     # print('case 2')
     try:
-        check_res = checklist(args.date)
-        if check_res[1] == 'date_ok':
+        inputted_date_analizer = check_date_length(args.date)
+        if inputted_date_analizer[1] == 'date_ok':
             # print('date_ok')
-            for k, v in sorted(my_dict.items()):
+            for k, v in sorted(parsed_data.items()):
                 if args.text is not None:
-                    if (k.__format__('%Y-%m-%d %H:%M:%S') == check_res[0].__format__('%Y-%m-%d %H:%M:%S')
-                            and v.lower().__contains__(args.text)):
+                    if (k.__format__('%Y-%m-%d %H:%M:%S') == inputted_date_analizer[0].__format__('%Y-%m-%d %H:%M:%S')
+                            and args.text in v.lower()):
+                            # and v.lower().__contains__(args.text)):
                         print(k, v)
                 elif args.text is None:
-                    if k.__format__('%Y-%m-%d %H:%M:%S') == check_res[0].__format__('%Y-%m-%d %H:%M:%S'):
+                    if k.__format__('%Y-%m-%d %H:%M:%S') == inputted_date_analizer[0].__format__('%Y-%m-%d %H:%M:%S'):
                         print(k, v)
 
-        if check_res[1] == 'more':
-            for k, v in sorted(my_dict.items()):
+        if inputted_date_analizer[1] == 'more':
+            for k, v in sorted(parsed_data.items()):
                 if args.text is not None:
-                    if k > check_res[0] and v.lower().__contains__(args.text):
+                    if k > inputted_date_analizer[0] and args.text in v.lower():
+                    # if k > check_res[0] and v.lower().__contains__(args.text):
                         print(k, v)
                 elif args.text is None:
-                    if k > check_res[0]:
+                    if k > inputted_date_analizer[0]:
                         print(k, v)
                         # print('more then:')
                         # print(check_res[0])
-        if check_res[1] == 'less':
+        if inputted_date_analizer[1] == 'less':
             # print('less')
-            for k, v in sorted(my_dict.items()):
+            for k, v in sorted(parsed_data.items()):
                 if args.text is not None:
-                    if k < check_res[0] and v.lower().__contains__(args.text):
+                    if k < inputted_date_analizer[0] and args.text in v.lower():
+                    # if k < check_res[0] and v.lower().__contains__(args.text):
                         print(k, v)
                 elif args.text is None:
-                    if k < check_res[0]:
+                    if k < inputted_date_analizer[0]:
                         print(k, v)
                     # print('less then:')
                     # print(check_res[0])
-        if check_res[1] == 'range_d':
+        if inputted_date_analizer[1] == 'range_d':
             if args.text is not None:
                 # print('range')
-                for k, v in sorted(my_dict.items()):
-                    if check_res[0] < k < check_res[2] and v.lower().__contains__(args.text):
+                for k, v in sorted(parsed_data.items()):
+                    # if check_res[0] < k < check_res[2] and v.lower().__contains__(args.text):
+                    if inputted_date_analizer[0] < k < inputted_date_analizer[2] and args.text in v.lower():
                         print(k, v)
             elif args.text is None:
-                for k, v in sorted(my_dict.items()):
-                    if check_res[0] < k < check_res[2]:
+                for k, v in sorted(parsed_data.items()):
+                    if inputted_date_analizer[0] < k < inputted_date_analizer[2]:
                         print(k, v)
     except ValueError:
         print('Ввести дату необходимо в формате: 31-12-2023-23-59-59')
         print('Разделитель должен быть один следующих: . , / - : ')
     except TypeError:
-        print('Не введен разделитель, разделитель должен быть один следующих: . , / - : ')
+        # print('Не введен разделитель, разделитель должен быть один следующих: . , / - : ')
+        print('Ввести дату необходимо cо временем, в формате: 31-12-2023-23-59-59')
 # case 3 (WITHOUT DATE, WITHOUT TEXT)
 elif args.date is None and args.text is None:
     # print('case 3')
-    for k, v in sorted(my_dict.items()):
+    for k, v in sorted(parsed_data.items()):
         print(k, v)
 
 # case 4 (WITH TEXT WITHOUT DATE)
 if args.text is not None and args.date is None:
     # print('case 4')
-    for k, v in sorted(my_dict.items()):
+    for k, v in sorted(parsed_data.items()):
         lines = v.splitlines()
         counter = 0
         for line in lines:
-            if line.lower().__contains__(args.text):
+            # if line.lower().__contains__(args.text):
+            if args.text in line.lower():
                 l2 = find_all_indexes(line.lower(), args.text)
                 for lin in l2:
                     counter += 1
